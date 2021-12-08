cce744c17f0580ffdaff7474b99b215ec5680709
Nika Jamburia <nikajamburia161@gmail.com>
2021-12-08 20:40:41 +0400

    finished Overview section

---
+++ navbar.js
@@ -0,0 +1,6 @@
+Array.from(document.getElementsByClassName('nav-link')).forEach(element => {
+    element.addEventListener('click', event => {
+        Array.from(document.getElementsByClassName('nav-link')).forEach(el => el.classList.remove('nav-link-active'))
+        event.target.classList.add('nav-link-active')
+    })
+});
